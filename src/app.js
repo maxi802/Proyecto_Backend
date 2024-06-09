9b98ab5e2929fc6cf18a5273ae3528eb1cd98ff4
maximartinez <bboymaxi17@gmail.com>
2024-06-09 19:10:38 -0300

    Segunda Preentrega Backend

--- src/app.js
+++ src/app.js
@@ -1,77 +1,32 @@
 import express from "express";
-import userRoutes from "./router/users.routes.js"
-import router from "./router/index.routes.js"
+import routes from "./routes/index.js";
 import __dirname from "./dirname.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import viewsRoutes from "./routes/views.routes.js";
 
-//Inicializamos express y la variable app contendra todas las funcionalidades de express
 const app = express();
-const PORT=8080;
 
-;
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.engine("handlebars", handlebars.engine()); // Inicia el motor del la plantilla
+app.set("views", __dirname + "/views"); // Indicamos que ruta se encuentras las vistas
+app.set("view engine", "handlebars"); // Indicamos con que motor vamos a utilizar las vistas
 app.use(express.static("public"));
 
-//Routes
-app.use("/api",router);
-console.log(__dirname);
+// Rutas de la api
+app.use("/api", routes);
 
+// Ruta de las vistas
+app.use("/", viewsRoutes)
 
-app.listen(8080, ()=>{
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-})
+const httpServer = app.listen(8080, () => {
+  console.log("Servidor escuchando en el puerto 8080");
+});
 
+// Configuramos socket
+export const io = new Server(httpServer);
 
-//actividad
-
-/*app.get("/bienvenida",(req, res)=>{
-    res.send("Bienvenido a mi primer servidor con express")
-})
-app.get("/saludo",(req, res)=>{
-    res.send("Mi primer respuesta del servidor con express")
-})
-app.get("/usuario",(req,res)=>{
-    const usuario = {
-        nombre:"Juan",
-        apellido:"Perez",
-        edad: 33,
-        correo:"jp@gmail.com"
-    }
-    res.send(usuario);
-})*/
-
-
-//actividad 2 - usuarios
-
-/*const usuarios =[
-    {id:1,nombre:"Miguel",apellido:"Castro",edad:54},
-    {id:2,nombre:"Juan",apellido:"Perez",edad:53},
-    {id:3,nombre:"Pepe",apellido:"Sapo",edad:54},
-]
-
-app.get("/" , (req , res)=>{
-    res.send(usuarios);
-})
-
-app.get("/usuarios/:id", (req,res)=>{
-    const {id}=req.params;
-
-    const usuario=usuarios.find(usuario=>usuario.id===Number(id));
-    if(!usuario) return res.send(`No se encuentra el usuario con el ID: ${id}`);
-    res.send(usuario);
-})
-*/
-
-
-//Mascotas
-
-/*let pets=[];
-app.get("/api/pets",(req,res)=>{
-    res.status(200).json(pets)
-})
-
-app.post("/api/pets",(req,res)=>{
-    const pet=req.body;
-    pets.push(pet);
-    res.status(201).json({status:"success",msg:"Mascota guardado"});
-})*/
+io.on("connection", (socket) => {
+  console.log("Nuevo usuario Conectado");
+});
